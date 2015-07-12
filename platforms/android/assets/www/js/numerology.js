526cabd99b2144c80db968457c10280ce5847cab
SmartDroidies <smartdroidies@gmail.com>
2015-07-12 09:15:51 +0530

    Menu Controls Added

--- platforms/android/assets/www/js/numerology.js
+++ platforms/android/assets/www/js/numerology.js
@@ -8,7 +8,7 @@ function onDeviceReadyAction() {
 			hidePopup();
 			//$("#menu").hide(200);
 		} else {
-			$("#menu").show(400);
+			$("#menu").show(350);
 			$("#setting").hide(200);
 		}
 	});
@@ -19,7 +19,7 @@ function onDeviceReadyAction() {
 			hidePopup();	
 			//$("#setting").hide(200);
 		} else {
-			$("#setting").show(400);
+			$("#setting").show(350);
 			$("#menu").hide(200);
 		}
 	});
@@ -37,4 +37,31 @@ function hideMenu() {
 
 function hideSetting() {
 	$("#setting").hide(200);
+}
+//Share the app link with user
+function share() {
+	window.plugins.socialsharing.share('Try this great Tamil App - ', '1500+ Tamil Tips',null,'https://play.google.com/store/apps/details?id=com.smart.droid.tamil.tips');
+	hidePopup();
+}
+
+//Provide Feedback
+function feedback() {
+	window.plugin.email.open({
+		to:      ['tips2stayhealthy@gmail.com'],
+		subject: 'Feedback on Tamil Kuripugal V0.2.3',
+		body:    '',
+		isHtml:  true
+	});
+	hidePopup();
+}
+//Rate App
+function rate() {
+	var version = device.platform;
+	hidePopup();
+	if(version == "Android") {
+		var url = "market://details?id=com.smart.droid.tamil.tips";
+        window.open(url,"_system");		
+	} else {
+		//var url = "https://play.google.com/store/apps/details?id=com.smart.droid.telugu.tips"
+	}	
 }
